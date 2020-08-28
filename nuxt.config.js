e61ca15924425f118d40c1c5e22de6e0bf27455d
daaef <realafe21@gmail.com>
2020-08-28 04:56:57 +0100

    pushing to my

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,6 +17,13 @@ export default {
   ** See https://nuxtjs.org/api/configuration-target
   */
   target: 'static',
+  /*
+   ** Headers of the page
+   ** See https://nuxtjs.org/api/configuration-head
+   */
+  generate: {
+    fallback: true,
+  },
 
   /*
   ** Headers of the page
